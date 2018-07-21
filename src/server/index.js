db8c17692e3199b8842d1847f9404fb7851de8fc
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-07-21 11:40:26 -0600

    Add basic client config stuffs

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,40 @@
 const express = require('express')
 const helmet = require('helmet')
 const logger = require('morgan')
+const serialize = require('serialize-javascript')
 
-const { NODE_ENV, PORT } = process.env
+const { ASSETS_BASE_URL, NODE_ENV, PORT } = process.env
 
 const app = express()
 
 const { readFileSync } = require('fs')
 const { join } = require('path')
 
+function getFilteredClientConfigObj (configObj) {
+  return {
+    process: {
+      env: Object.entries(configObj)
+        .filter(([key]) => key.startsWith('CLIENT_'))
+        .reduce((accum, [key, value]) => ({ ...accum, [key]: value }), {})
+    }
+  }
+}
+
+function getConfigScriptTag (configObj) {
+  return `<script>window.global = ${serialize(config)}</script>`
+}
+
+const config = getFilteredClientConfigObj(global.process.env)
 const html = readFileSync(join(process.cwd(), 'dist/index.html'))
   .toString()
-  .replace(/"\//g, '"http://localhost:1234/')
+  .replace(/"\//g, `"${ASSETS_BASE_URL}/`)
+  .replace('<!-- CONFIGURATION -->', getConfigScriptTag(config))
 
 app.use(helmet())
 app.use(logger('dev'))
 
-app.get('*', (req, res) => {
-  res.type('text/html')
-  res.end(html)
-})
+app.get('*', (req, res) => res.send(html))
 
-NODE_ENV !== 'test' && app.listen(PORT, '0.0.0.0', () => {
+NODE_ENV !== 'test' && app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
 })
