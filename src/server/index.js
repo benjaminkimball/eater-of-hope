2c15ff99a8beb19108bec1619d558112a5eae732
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-07-24 15:59:32 -0600

    Abstract renderClient() middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,21 @@
 const express = require('express')
 const helmet = require('helmet')
 const logger = require('morgan')
-const serialize = require('serialize-javascript')
+const { join } = require('path')
+
+const renderClient = require('./middleware/render-client')
 
 const { ASSETS_BASE_URL, NODE_ENV, PORT } = process.env
 
 const app = express()
 
-const { readFileSync } = require('fs')
-const { join } = require('path')
-
-function getConfig (configObj) {
-  return {
-    process: {
-      env: Object.entries(configObj)
-        .filter(([key]) => key.startsWith('CLIENT_'))
-        .reduce((accum, [key, value]) => ({ ...accum, [key]: value }), {})
-    }
-  }
-}
-
-function getConfigScriptTag (configObj) {
-  return `<script>window.global = ${serialize(config)}</script>`
-}
-
-const config = getConfig(global.process.env)
-const html = readFileSync(join(process.cwd(), 'dist/index.html'))
-  .toString()
-  .replace(/"\//g, `"${ASSETS_BASE_URL}/`)
-  .replace('<!-- CONFIGURATION -->', getConfigScriptTag(config))
-
 app.use(helmet())
 app.use(logger('dev'))
 
-app.get('*', (req, res) => res.send(html))
+app.get('*', renderClient({
+  assetsUrl: ASSETS_BASE_URL,
+  path: join(process.cwd(), 'dist/index.html')
+}))
 
 NODE_ENV !== 'test' && app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`)
