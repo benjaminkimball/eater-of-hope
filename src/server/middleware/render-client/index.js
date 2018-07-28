c4deb1b4213db12e24d2df830f55bff197c47231
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-07-28 14:14:05 -0600

    Add TODOs and prep middleware for handling options argument

--- src/server/middleware/render-client/index.js
+++ src/server/middleware/render-client/index.js
@@ -4,14 +4,18 @@ const { join } = require('path')
 const configScriptTag = require('./config-script-tag')
 const filterConfig = require('./filter-config')
 
+// TODO: Pass in as an option to middleware
 const { ASSETS_BASE_URL } = process.env
 
+// TODO: Pass config object as an option to middleware
 const config = filterConfig(global.process.env)
-const path = join(process.cwd(), 'dist/index.html')
-const html = readFileSync(path).toString()
+// TODO: Pass file or file.toString() as an option to middleware
+const file = readFileSync(join(process.cwd(), 'dist/index.html'))
+// TODO: Abstract into own file with tests
+const html = file.toString()
   .replace(/"\//g, `"${ASSETS_BASE_URL}/`)
   .replace('<!-- CONFIG -->', configScriptTag(config))
 
-module.exports = function renderClient () {
+module.exports = function renderClient (options = {}) {
   return (req, res) => res.send(html)
 }
