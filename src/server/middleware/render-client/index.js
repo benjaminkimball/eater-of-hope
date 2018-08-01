fda90c1dd05529f53aff7fbb74533e1f0585ce43
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-08-01 09:26:34 -0600

    Stream renderClient() response

--- src/server/middleware/render-client/index.js
+++ src/server/middleware/render-client/index.js
@@ -6,6 +6,10 @@ module.exports = function renderClient ({ assetsBaseUrl, config, html }) {
   const url = `"${assetsBaseUrl}/`
   const tag = `<script>window.__CONFIG__ = ${serialize(filterConfig(config))}</script>`
   const response = html.replace(/"\//g, url).replace('<!-- CONFIG -->', tag)
+  const [htmlStart, htmlEnd] = response.split('<!-- ROOT -->')
 
-  return (req, res) => res.send(response)
+  return (req, res) => {
+    res.type('html').write(htmlStart)
+    res.end(htmlEnd)
+  }
 }
