16ad7095e0fa63edf89c4faf51b1f2873f22b8fa
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-08-25 20:30:25 -0600

    Update tool configs and dependencies, clean index.html

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: [
-    '<rootDir>/.cache/',
-    '<rootDir>/coverage/',
-    '<rootDir>/dist/',
-    '<rootDir>/node_modules/'
-  ],
   // NOTE: Needed to test the server's CommonJS modules!
   testURL: 'http://localhost/'
 }
