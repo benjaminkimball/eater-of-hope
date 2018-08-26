0efca12595932f9e0c4bde6c0846173accf08ba0
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-08-25 21:37:46 -0600

    Add getConfig() selector

--- src/client/reducers/config.js
+++ src/client/reducers/config.js
@@ -1,3 +1,7 @@
+import { get } from 'lodash'
+
 export default function config (state = {}, action) {
   return state
 }
+
+export const getConfig = (state) => get(state, 'config', {})
--- src/client/reducers/config.spec.js
+++ src/client/reducers/config.spec.js
@@ -1,4 +1,4 @@
-import reducer from './config'
+import reducer, { getConfig } from './config'
 
 describe('config()', () => {
   it('returns an empty object by default', () => {
@@ -7,5 +7,17 @@ describe('config()', () => {
 
   it('returns supplied initial state', () => {
     expect(reducer({ foo: true }, {})).toEqual({ foo: true })
+  })
+})
+
+describe('getConfig()', () => {
+  it('returns an empty object by default', () => {
+    expect(getConfig()).toEqual({})
+  })
+
+  it('returns the raw config branch of the store', () => {
+    const state = { config: { CLIENT_TEST: 'foo' } }
+
+    expect(getConfig(state)).toEqual(state.config)
   })
 })
