9378c0a486ff9ce27ea2ed7aa6097f9f0f4b9583
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-09-04 18:10:40 -0600

    Remove styled-components

--- src/client/app.js
+++ src/client/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import styled from 'styled-components'
 
 import configureStore from './configure-store'
 
@@ -10,13 +9,9 @@ const store = configureStore(window.__INITIAL_STATE__)
 // NOTE: We have a copy in the store, let the original get garbage collected
 delete window.__INITIAL_STATE__
 
-const Header = styled.h1`
-  color: #f08;
-`
-
 class App extends Component {
   render () {
-    return <Header>Hey.</Header>
+    return <h1>Hey.</h1>
   }
 }
 
