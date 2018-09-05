1222efc0f5093a169424c7d0184794d31aefd129
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-09-04 18:39:38 -0600

    Simplify styles

--- src/client/components/grid.js
+++ src/client/components/grid.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import './styles.css'
-
 export const Grid = ({ children }) =>
   <div className='o-grid'>{children}</div>
 
--- src/client/components/layout.js
+++ src/client/components/layout.js
@@ -1,8 +1,6 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import './styles.css'
-
 export const Layout = ({ children }) =>
   <div className='o-layout'>{children}</div>
 
