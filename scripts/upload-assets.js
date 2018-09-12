507cf43ee40cb0a00cce3981d7424d19527e5a0b
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-09-12 16:31:27 -0600

    Add retryable tasks

--- scripts/upload-assets.js
+++ scripts/upload-assets.js
@@ -1,10 +1,12 @@
 const parallelLimit = require('async/parallelLimit')
+const retryable = require('async/retryable')
 const { readdir } = require('fs')
 const { Storage } = require('@google-cloud/storage')
 const { join } = require('path')
 const { promisify } = require('util')
 
 const readDir = promisify(readdir)
+const runParallelTasks = promisify(parallelLimit)
 
 const distDir = join(process.cwd(), 'dist')
 
@@ -13,29 +15,16 @@ const bucket = storage.bucket('assets')
 
 readDir(distDir)
   .then(files => files.filter(file => /(\.css|\.js|\.map)$/.test(file)))
-  .then(files => files.map(file => (callback) => {
-    console.log(`  ${file}`)
-
+  .then(files => files.map(file => retryable(2, callback => {
     bucket
       .upload(join(distDir, file), {
         cacheControl: 'public, max-age=31536000',
         gzip: true,
         public: true
       })
-      .then(() => callback(null, file))
-      .catch((err) => callback(err))
-  }))
-  .then(tasks => {
-    console.log('Uploading:')
-
-    parallelLimit(tasks, 2, (err, result) => {
-      if (err) {
-        console.error('Failed:')
-        console.log(`  ${err.message}`)
-      } else {
-        console.log('Uploaded:')
-        console.log(`  ${result.join('\n  ')}`)
-      }
-    })
-  })
+      .then(() => callback())
+      .catch(callback)
+  })))
+  .then(tasks => runParallelTasks(tasks, 2))
+  .then(console.log)
   .catch(console.error)
