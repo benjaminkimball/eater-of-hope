dfb958870e3fa265015e0c360b1ad26d868ff502
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-09-17 11:40:36 -0600

    Add winston for logging, remove morgan

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const { readFileSync } = require('fs')
 const helmet = require('helmet')
-const logger = require('morgan')
 const { join } = require('path')
+
+const logging = require('../../lib/logging')
 
 const renderClient = require('./middleware/render-client')
 
 const app = module.exports = express()
 
 app.use(helmet())
-app.use(logger('dev'))
+app.use(logging.requestLogger)
 
 const { ASSETS_BASE_URL: assetsBaseUrl } = process.env
 const config = process.env
 const html = readFileSync(join(process.cwd(), 'dist/index.html')).toString()
 
 app.get('*', renderClient({ assetsBaseUrl, config, html }))
+
+app.use(logging.errorLogger)
