b4c51e87645abaefa708a0a45ab82d9772223b81
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-09-21 10:27:05 -0600

    Consolidated server entrypoint files

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,26 @@
-const app = require('./server')
+const express = require('express')
+const { readFileSync } = require('fs')
+const helmet = require('helmet')
+const { join } = require('path')
 
-app.listen(5000, () => {
-  console.log(`Server running at http://localhost:5000`)
-})
+const logging = require('../../lib/logging')
+
+const renderClient = require('./middleware/render-client')
+
+const app = module.exports = express()
+
+app.use(helmet())
+app.use(logging.requestLogger)
+
+const { ASSETS_BASE_URL: assetsBaseUrl } = process.env
+const html = readFileSync(join(process.cwd(), 'dist/index.html')).toString()
+
+app.get('*', renderClient({ assetsBaseUrl, html }))
+
+app.use(logging.errorLogger)
+
+if (module === require.main) {
+  app.listen(5000, () => {
+    console.log(`Server running at http://localhost:5000`)
+  })
+}
