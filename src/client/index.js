49f7ed0ac1706b81d0f175e9b8902a6e4aeebba7
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-10-09 16:59:09 -0600

    Remove a lot of stuffs

--- src/client/index.js
+++ src/client/index.js
@@ -1,15 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-
-import configureStore from './configure-store'
 
 import App from './components/app'
 
-const store = configureStore()
-
-render((
-  <Provider store={store}>
-    <App />
-  </Provider>
-), document.getElementById('root'))
+render(<App />, document.getElementById('root'))
