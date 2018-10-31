adde182a2c432bb0415a806e4a03bcdbdd670298
Benjamin Kimball <benjamin.r.kimball@gmail.com>
2018-10-31 10:13:19 -0600

    Correct quotes on startup log

--- src/server/index.js
+++ src/server/index.js
@@ -22,6 +22,6 @@ app.use(logging.errorLogger)
 
 if (module === require.main) {
   app.listen(5000, () => {
-    console.log(`Server running at http://localhost:5000`)
+    console.log('Server running at http://localhost:5000')
   })
 }
